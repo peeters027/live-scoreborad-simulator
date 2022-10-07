fa0eb2051c7cfa0ba64cde69fb0dc704983ddc52
Szmolke, Piotr <piotr.szmolke@six-group.com>
2022-10-07 17:37:39 +0200

    Starting an application with start.bat done

---
+++ src/main/java/pl/szmolke/LiveScoreboardSimulator.java
@@ -0,0 +1,9 @@
+package pl.szmolke;
+
+public class LiveScoreboardSimulator {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Live Scoreboard Simulator");
+        new java.util.Scanner(System.in).nextLine();
+    }
+}
---
+++ start.bat
@@ -0,0 +1,2 @@
+cd target
+java -jar live-scoreboard-simulator-1.0-SNAPSHOT.jar
