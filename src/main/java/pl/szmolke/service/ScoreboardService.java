1c3272f1577c1a9b2565a963d2115eaaf7576a57
Szmolke, Piotr <piotr.szmolke@six-group.com>
2022-10-07 18:21:44 +0200

    Test for starting the match when at least 1 provided team is currently in play PASSED

--- src/main/java/pl/szmolke/service/ScoreboardService.java
+++ src/main/java/pl/szmolke/service/ScoreboardService.java
@@ -10,6 +10,18 @@ public class ScoreboardService {
 
     public Match startMatch(String homeTeam, String guestTeam) {
 
+        boolean atLeastOneTeamAlreadyInPlay = InMemoryDB.MATCHES
+                .stream()
+                .anyMatch(m -> m.getHomeTeam().equals(homeTeam)
+                        || m.getHomeTeam().equals(guestTeam)
+                        || m.getGuestTeam().equals(homeTeam)
+                        || m.getGuestTeam().equals(guestTeam));
+
+        if (atLeastOneTeamAlreadyInPlay) {
+            System.out.println("Match was not added. At least one of the provided team is currently playing.");
+            return null;
+        }
+
         Match match = Match.builder()
                 .homeTeam(homeTeam)
                 .guestTeam(guestTeam)
