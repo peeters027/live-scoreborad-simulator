53796bd9c33884cb557d3189ac3c075bc542e441
Szmolke, Piotr <piotr.szmolke@six-group.com>
2022-10-07 18:15:57 +0200

    Test for starting the match when at least 1 provided team is currently in play

--- src/test/java/ScoreboardServiceTest.java
+++ src/test/java/ScoreboardServiceTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pl.szmolke.database.InMemoryDB;
 import pl.szmolke.model.Match;
 import pl.szmolke.service.ScoreboardService;
+
+import java.util.Arrays;
 
 class ScoreboardServiceTest {
 
@@ -11,14 +14,33 @@ class ScoreboardServiceTest {
     @BeforeEach
     void setUp() {
         scoreboardService = new ScoreboardService();
+        initInMemoryDb();
+    }
+
+    private void initInMemoryDb() {
+        InMemoryDB.MATCHES.addAll(Arrays.asList(
+                Match.builder()
+                        .homeTeam("Spain")
+                        .guestTeam("Brazil")
+                        .homeScore(10)
+                        .guestScore(2)
+                        .build(),
+                Match.builder()
+                        .homeTeam("Mexico")
+                        .guestTeam("Canada")
+                        .homeScore(0)
+                        .guestScore(5)
+                        .build()
+        ));
     }
 
     @Test
-    void itShouldStartMatchWithInitialScore() {
+    void itShouldStartMatchWithInitialScoreWhenTeamsAreNotCurrentlyPlaying() {
 
         // given
         String homeTeam = "Poland";
         String guestTeam = "Germany";
+        int numberOfMatchesInPlayBeforeAdding = InMemoryDB.MATCHES.size();
 
         // when
         Match startedMatch = scoreboardService.startMatch(homeTeam, guestTeam);
@@ -28,5 +50,22 @@ class ScoreboardServiceTest {
         Assertions.assertEquals(guestTeam, startedMatch.getGuestTeam());
         Assertions.assertEquals(0, startedMatch.getHomeScore());
         Assertions.assertEquals(0, startedMatch.getGuestScore());
+        Assertions.assertEquals(numberOfMatchesInPlayBeforeAdding + 1, InMemoryDB.MATCHES.size());
+    }
+
+    @Test
+    void itShouldNorStartMatchWhenAtLestOneTeamIsCurrentlyPlaying() {
+
+        // given
+        String homeTeam = "Spain";
+        String guestTeam = "Germany";
+        int numberOfMatchesInPlayBeforeAdding = InMemoryDB.MATCHES.size();
+
+        // when
+        Match startedMatch = scoreboardService.startMatch(homeTeam, guestTeam);
+
+        // then
+        Assertions.assertNull(startedMatch);
+        Assertions.assertEquals(numberOfMatchesInPlayBeforeAdding, InMemoryDB.MATCHES.size());
     }
 }
