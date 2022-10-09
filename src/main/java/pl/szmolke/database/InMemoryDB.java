7f3bb1c9b73045148caa4bda7f79681345d934c0
Szmolke, Piotr <piotr.szmolke@six-group.com>
2022-10-09 12:40:17 +0200

    Insert init dummy data to InMemoryDB

--- src/main/java/pl/szmolke/database/InMemoryDB.java
+++ src/main/java/pl/szmolke/database/InMemoryDB.java
@@ -1,11 +1,45 @@
 package pl.szmolke.database;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import pl.szmolke.model.Match;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class InMemoryDB {
 
-    public static List<Match> MATCHES = new ArrayList<>();
+    public static List<Match> MATCHES = new ArrayList<>(
+            Arrays.asList(
+                    Match.builder()
+                            .homeTeam("Mexico")
+                            .guestTeam("Canada")
+                            .homeScore(0)
+                            .guestScore(5)
+                            .build(),
+                    Match.builder()
+                            .homeTeam("Spain")
+                            .guestTeam("Brazil")
+                            .homeScore(10)
+                            .guestScore(2)
+                            .build(),
+                    Match.builder()
+                            .homeTeam("Germany")
+                            .guestTeam("France")
+                            .homeScore(2)
+                            .guestScore(2)
+                            .build(),
+                    Match.builder()
+                            .homeTeam("Uruguay")
+                            .guestTeam("Italy")
+                            .homeScore(6)
+                            .guestScore(6)
+                            .build(),
+                    Match.builder()
+                            .homeTeam("Argentina")
+                            .guestTeam("Australia")
+                            .homeScore(3)
+                            .guestScore(1)
+                            .build()
+            )
+    );
 }
