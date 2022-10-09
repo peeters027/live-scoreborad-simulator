e37228dbdac169d3c7b3d5276a423b931b15840e
Szmolke, Piotr <piotr.szmolke@six-group.com>
2022-10-09 13:01:20 +0200

    Starting a game in menu implemented - first manual tests done

--- src/main/java/pl/szmolke/LiveScoreboardSimulator.java
+++ src/main/java/pl/szmolke/LiveScoreboardSimulator.java
@@ -1,5 +1,6 @@
 package pl.szmolke;
 
+import pl.szmolke.exception.TeamNameFormatException;
 import pl.szmolke.service.ScoreboardService;
 
 import java.util.Scanner;
@@ -19,11 +20,20 @@ public class LiveScoreboardSimulator {
             System.out.print("5.) Exit\n");
             System.out.print("\nEnter Your Menu Choice: ");
 
-            int choice = input.nextInt();
+            int choice = Integer.parseInt(input.nextLine());
 
             switch (choice) {
                 case 1:
                     System.out.println("\nStarting a game... ");
+                    System.out.println("Provide a name of home team");
+                    String homeTeam = input.nextLine();
+                    System.out.println("Provide a name of guest team");
+                    String guestTeam = input.nextLine();
+                    try {
+                        scoreboardService.startMatch(homeTeam, guestTeam);
+                    } catch (TeamNameFormatException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 2:
                     System.out.println("\nFinishing a game...");
@@ -33,10 +43,14 @@ public class LiveScoreboardSimulator {
                     break;
                 case 4:
                     System.out.println("\nSummary of games...");
+                    scoreboardService.getSummaryOfGames();
                     break;
                 case 5:
                     System.out.println("\nExiting Program...");
                     System.exit(0);
+                    break;
+                default:
+                    System.out.println("This is not a valid menu option! Please select again.");
                     break;
             }
         }
